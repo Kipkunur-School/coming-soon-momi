e54f32a129e101c514fcccb8537d0abda7156e24
Ezrqn Kemboi <ezrqnkemboi@gmail.com>
2024-04-09 15:57:20 +0300

    test change

--- src/components/__tests__/ComingSoon.spec.ts
+++ src/components/__tests__/ComingSoon.spec.ts
@@ -1,11 +1,11 @@
 import { describe, it, expect } from 'vitest'
 
-import { shallow } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
 import ComingSoon from '@/components/ComingSoon.vue'
 
 describe('ComingSoon', () => {
   it('renders properly', () => {
-    const wrapper = shallow(ComingSoon)
+    const wrapper = mount(ComingSoon)
     const h1= wrapper.find('h1')
     expect(h1.text()).toBe('WE\'RE COMING SOON!')
   })
